82866a474678dc628eb916f63e21482087c3ca60
vboxuser <vboxuser@Linux.myguest.virtualbox.org>
2024-06-20 14:29:54 -0700

    updated homepage

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,3 +1,7 @@
+import fb from "../images/fb.png";
+import yt from "../images/yt.png";
+import whatsapp from "../images/whatsapp.png";
+
 const Footer = () => {
     return (
         <footer>
@@ -13,7 +17,9 @@ const Footer = () => {
                      <p></p>
                      <a href="/contact-us" className="space btn">Contact Us</a>
                      <p></p>
-                     <a href="https://www.facebook.com/ShirdiSaiDhamBellevue" className="space btn">Follow Us</a>
+                     <a href="https://www.facebook.com/ShirdiSaiDhamBellevue"><img src={fb} alt="" className="external space" /></a>
+                     <a href="https://www.youtube.com/@shirdisaidham2902"><img src={yt} alt="" className="external" /></a>
+                     <a href="https://chat.whatsapp.com/GY0E24mpiiCLtDNf7cLWze"><img src={whatsapp} alt="" className="external2" /></a>
                  </div>
              </footer>
     )
